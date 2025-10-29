e2a74ffd27edf68037f37b4ac0d4961d0a7060c5
jishnu <jishnusathyanarayanan@gmail.com>
2025-10-30 01:26:48 +0530

    update database.py of the sqlalchemy_database)url

--- fastapi_backend/database.py
+++ fastapi_backend/database.py
@@ -6,10 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQL_DATABASE_URL = "postgresql://jishnu04:shyjasathyan17@localhost:5432/my_database"
+# SQL_DATABASE_URL = "postgresql://jishnu04:shyjasathyan17@localhost:5432/my_database"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
 engine = create_engine(
-    SQL_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessioLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
