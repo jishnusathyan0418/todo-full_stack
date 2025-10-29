9b3d65aadcca8514eb4268621c9f877ffd96cc01
jishnu <jishnusathyanarayanan@gmail.com>
2025-10-30 01:59:36 +0530

    changes made in config,py

--- fastapi_backend/config.py
+++ fastapi_backend/config.py
@@ -1,13 +1,10 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_HOST: str
-    DATABASE_NAME: str
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str 
-    DATABASE_PORT: int
-    
+
     class Config():
         env_file = ".env"
         extra = "ignore"
         
+        
+settings = Settings()
