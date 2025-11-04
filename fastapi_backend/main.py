8f856cb5a7238b08b44c11ddc02c8da109d7be66
jishnu <jishnusathyanarayanan@gmail.com>
2025-11-04 15:17:42 +0530

    corrected the errors:

--- fastapi_backend/main.py
+++ fastapi_backend/main.py
@@ -26,7 +26,7 @@ app.include_router(todos.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
