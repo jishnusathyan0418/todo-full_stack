d42b6617d15f0c7ed2b7a14a020dbfba25060760
jishnu <jishnusathyanarayanan@gmail.com>
2025-11-04 16:43:30 +0530

    last

--- fastapi_backend/main.py
+++ fastapi_backend/main.py
@@ -26,7 +26,7 @@ app.include_router(todos.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
+    allow_origins=["https://localhost:3000",
                    "https://todo-full-stack-six.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
